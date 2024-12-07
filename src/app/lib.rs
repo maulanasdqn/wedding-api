06327481b4b73404bc0482ba4795f602e438bae6
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-12-07 20:33:29 +0700

    chore: re-structure project

--- src/app/lib.rs
+++ src/app/lib.rs
@@ -1,6 +1,6 @@
 mod auth;
 mod reservations;
-use auth::auth_middleware::authorization_middleware;
+use auth::{auth_middleware::authorization_middleware, auth_router};
 use axum::{http::HeaderValue, middleware::from_fn, Router};
 use reservations::reservation_router;
 use std::env;
@@ -10,17 +10,19 @@ pub fn routes() -> Router {
 	let origin = env::var("CORS_ORIGIN").expect("CORS_ORIGIN must be set");
 	let cors_middleware =
 		CorsLayer::new().allow_origin(origin.parse::<HeaderValue>().unwrap());
-	let auth_middleware = from_fn(authorization_middleware);
-
-	let protected_routes = Router::new()
-		.nest("/reservations", reservation_router().layer(auth_middleware));
 
 	Router::new()
 		.nest(
 			"/api",
 			Router::new()
-				.nest("", protected_routes)
-				.nest("/auth", auth::auth_router()),
+				.nest("", protected_routes())
+				.nest("/auth", auth_router()),
 		)
 		.layer(cors_middleware)
+}
+
+fn protected_routes() -> Router {
+	Router::new()
+		.nest("/reservations", reservation_router())
+		.layer(from_fn(authorization_middleware))
 }
