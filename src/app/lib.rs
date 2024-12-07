bdd6dc881eefe49e2e245307fb4c59d3d3dffd98
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-12-08 02:23:06 +0700

    chore: adjust CORS

--- src/app/lib.rs
+++ src/app/lib.rs
@@ -1,16 +1,28 @@
 mod auth;
 mod reservations;
+
 use auth::{auth_middleware::authorization_middleware, auth_router};
-use axum::{http::HeaderValue, middleware::from_fn, Router};
+use axum::{
+	http::{header, HeaderValue, Method},
+	middleware::from_fn,
+	Router,
+};
 use reservations::reservation_router;
 use std::env;
 use storage::upload_routes;
 use tower_http::cors::CorsLayer;
 
 pub async fn routes() -> Router {
-	let origin = env::var("CORS_ORIGIN").expect("CORS_ORIGIN must be set");
-	let cors_middleware =
-		CorsLayer::new().allow_origin(origin.parse::<HeaderValue>().unwrap());
+	let cors_origin = match env::var("RUST_ENV").as_deref() {
+		Ok("development") => "http://localhost:5173",
+		_ => "https://wedding.fenny.me",
+	};
+
+	let cors_middleware = CorsLayer::new()
+		.allow_origin(cors_origin.parse::<HeaderValue>().unwrap())
+		.allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+		.allow_headers([header::AUTHORIZATION, header::CONTENT_TYPE])
+		.allow_credentials(true);
 
 	Router::new()
 		.nest(
