4f4d54b3f631da6a06164804d97b9ac470f98414
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-12-07 23:18:06 +0700

    chore: make minio https

--- src/libs/storage/service.rs
+++ src/libs/storage/service.rs
@@ -26,7 +26,7 @@ impl MinioClient {
 		let client = Minio::builder()
 			.endpoint(&endpoint)
 			.provider(provider)
-			.secure(false)
+			.secure(true)
 			.build()?;
 
 		if !client.bucket_exists(&bucket_name).await? {
@@ -75,7 +75,7 @@ impl MinioClient {
 			.await?;
 
 		let file_url = format!(
-			"http://{}/{}/{}",
+			"https://{}/{}/{}",
 			endpoint, self.bucket_name, unique_filename
 		);
 
